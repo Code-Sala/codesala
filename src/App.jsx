fda4b4660e3211bfddeb2b6e44afcc17bad45202
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-02-26 14:43:37 +0545

    new routes add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Layout from "./Layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
         </Route>
       </Routes>
     </Router>
