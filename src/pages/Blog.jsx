327a8dc30bd0e9dc69b39be533263794ec5da34c
bhaire7 <baibhavnepal07@gmail.com>
2025-03-05 17:38:23 +0545

    Paginaty Section Done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,23 +1,38 @@
+import React, { useState } from "react";
 import HeroSection from "../components/Blog/HeroSection";
 import NavSection from "../components/Blog/NavSection";
 import Pagecount from "../components/Blog/Pagecount";
 import SideBar from "../components/Blog/SideBar";
+import BlogData from "../Data/Blog.json";
 
 function Blog() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 4;
+
+  // Calculate indices for slicing the blog data
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = BlogData.slice(firstPostIndex, lastPostIndex);
+
   return (
     <div>
       <NavSection />
 
       <div className="grid lg:grid-cols-2 gap-6 w-full">
         <div>
-          <HeroSection />
+          <HeroSection posts={currentPosts} />
         </div>
         <div className="hidden lg:block">
           <SideBar />
         </div>
       </div>
 
-      <Pagecount />
+      <Pagecount
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPosts={BlogData.length}
+        postsPerPage={postsPerPage}
+      />
 
       {/* Move SideBar below Pagecount on small screens */}
       <div className="lg:hidden">
