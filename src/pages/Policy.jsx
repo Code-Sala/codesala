003ab87637b2a0c33bcd2e481962732429d8a221
bhaire7 <baibhavnepal07@gmail.com>
2025-03-10 14:36:30 +0545

    terms section done

---
+++ src/pages/Policy.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PolicyHero from "../components/Policy/PolicyHero";
+import PolicyBody from "../components/Policy/PolicyBody";
+
+const Policy = () => {
+  return (
+    <div>
+      <PolicyHero />
+      <PolicyBody />
+    </div>
+  );
+};
+
+export default Policy;
