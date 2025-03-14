a366fb8eefac8cc567fbb3df9ef1b55309f8c694
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-03-14 18:00:34 +0545

    contact section add

--- src/utils/GetInTouch.jsx
+++ src/utils/GetInTouch.jsx
@@ -1,9 +1,25 @@
-import { IoLogoWhatsapp } from "react-icons/io";
+import { FaWhatsapp } from "react-icons/fa";
 
 function GetInTouch() {
   return (
-    <div className="fixed bottom-25">
-      <IoLogoWhatsapp size={40} />
+    <div className="fixed right-5 bottom-25">
+      <div className="bg-white rounded-full hover:scale-110">
+        <a
+          href="tel:9841255592"
+          className=" p-2 flex items-center justify-center "
+        >
+          <svg width="40" height="40" viewBox="0 0 16 16">
+            <defs>
+              <linearGradient x1="0%" y1="0%" x2="100%" y2="100%">
+                <stop offset="0%" stopColor="#00AEEF" />
+                <stop offset="50%" stopColor="#892890" />
+                <stop offset="100%" stopColor="#ED1D7E" />
+              </linearGradient>
+            </defs>
+            <FaWhatsapp width={24} height={24} fill={`url(#gradient)`} />
+          </svg>
+        </a>
+      </div>
     </div>
   );
 }
