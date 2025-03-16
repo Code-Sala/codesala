465ac026eb5a19286110e4d8cb32ac9a838ca523
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2025-03-16 12:44:00 +0545

    linear added to whatsapp icon

--- src/utils/GetInTouch.jsx
+++ src/utils/GetInTouch.jsx
@@ -2,21 +2,25 @@ import { FaWhatsapp } from "react-icons/fa";
 
 function GetInTouch() {
   return (
-    <div className="fixed right-5 bottom-25">
+    <div className="fixed right-5 bottom-25 z-90">
       <div className="bg-white rounded-full hover:scale-110">
         <a
-          href="tel:9841255592"
-          className=" p-2 flex items-center justify-center "
+          href="https://wa.me/9841255592"
+          className="p-2 flex items-center justify-center"
         >
           <svg width="40" height="40" viewBox="0 0 16 16">
             <defs>
-              <linearGradient x1="0%" y1="0%" x2="100%" y2="100%">
+              <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="100%">
                 <stop offset="0%" stopColor="#00AEEF" />
                 <stop offset="50%" stopColor="#892890" />
                 <stop offset="100%" stopColor="#ED1D7E" />
               </linearGradient>
             </defs>
-            <FaWhatsapp width={24} height={24} fill={`url(#gradient)`} />
+            <FaWhatsapp
+              width={24}
+              height={24}
+              style={{ fill: "url(#gradient)" }}
+            />
           </svg>
         </a>
       </div>
