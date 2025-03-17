6c7acd6e63a03d5e4e8529da9d8fc9ee742fe7c6
bhaire7 <baibhavnepal07@gmail.com>
2025-03-17 15:11:15 +0545

    Dark theme done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,15 @@
-import ContactForm from "../components/Contact/ContactForm"
-import ContactLocation from "../components/Contact/ContactLocation"
+import ContactForm from "../components/Contact/ContactForm";
+import ContactLocation from "../components/Contact/ContactLocation";
 // import ContactHero from "../components/Contact/ContactHero"
 
 function Contact() {
   return (
-   <>
-   {/* <ContactHero/> */}
-   <ContactForm/>
-   <ContactLocation/>
-   </>
-  )
+    <div className="bg-white">
+      {/* <ContactHero/> */}
+      <ContactForm />
+      <ContactLocation />
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
